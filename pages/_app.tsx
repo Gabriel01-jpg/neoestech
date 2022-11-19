26f9c79cd896ef16cc688b9d3fc6412dbe84951c
Gabriel <gabriellima01.js@gmail.com>
2022-11-19 09:43:15 -0300

    Mobile login style and login logic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ConfigProvider } from 'antd';
+import { AuthProvider } from '../context/AuthProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ConfigProvider direction="ltr">
+      <AuthProvider >
+        <Component {...pageProps} className="font-sans" />
+      </AuthProvider>
+    </ConfigProvider>
+  )
 }
